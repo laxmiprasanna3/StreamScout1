d514111601bdac67b69d1bcf45e6834d7b1d9fed
kasireddy laxmi prasanna <145850752+laxmiprasanna3@users.noreply.github.com>
2024-06-11 07:59:55 +0000

    commit

---
+++ src/pages/Play.Page.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+// HOC
+import DefaultlayoutHoc from "../layout/Default.layout";
+
+const PlayPage = () => {
+  return <div>PlayPage</div>;
+};
+
+export default DefaultlayoutHoc(PlayPage);
